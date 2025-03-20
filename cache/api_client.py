7708eb63bdeb4193a72c0e49bb6ff0917b2ff403
Jonathan Govinda Medrano Salazar <arithgrey@gmail.com>
2025-03-19 20:05:29 -0600

    [FEAT] devops tools

--- cache/api_client.py
+++ cache/api_client.py
@@ -1,6 +1,9 @@
 from rest_framework.response import Response
 from rest_framework import status
 import requests
+import json
+import urllib.request
+import ssl
 from django.core.cache import cache
 
 class ApiClient:
@@ -12,11 +15,13 @@ class ApiClient:
     def top_sellers(self):
         try:
             url = f'{self.base_url}/productos/top-sellers/?format=json'
-            response = requests.get(url=url,verify=False)
-            response.raise_for_status()
-            return response.json()
-        except requests.RequestException as e:
-            raise Exception(f'Error al obtener los productos: {str(e)}')
+            context = ssl._create_unverified_context()
+            response = urllib.request.urlopen(url, context=context)
+            return json.loads(response.read().decode('utf-8'))
+        except Exception as e:
+            # En lugar de lanzar una excepción, devolver una lista vacía o datos predeterminados
+            print(f'Error al obtener los productos: {str(e)}')
+            return []  # Devolver lista vacía en caso de error
     
     def products_category(self, category_slug: str ) -> list:
         try:
