1c0a6575e13892db873608aa9cceaf897b8268ac
Jonathan Govinda Medrano Salazar <arithgrey@gmail.com>
2025-03-19 20:15:39 -0600

    [FEAT] devops tools

--- cache/api_client.py
+++ cache/api_client.py
@@ -11,12 +11,12 @@ class ApiClient:
     def __init__(self):
         self.base_url = 'http://localhost:8000/api/enid'
         self.headers = {}
+        self.context = ssl._create_unverified_context()
     
     def top_sellers(self):
         try:
             url = f'{self.base_url}/productos/top-sellers/?format=json'
-            context = ssl._create_unverified_context()
-            response = urllib.request.urlopen(url, context=context)
+            response = urllib.request.urlopen(url, context=self.context)
             return json.loads(response.read().decode('utf-8'))
         except Exception as e:
             # En lugar de lanzar una excepción, devolver una lista vacía o datos predeterminados
@@ -26,10 +26,9 @@ class ApiClient:
     def products_category(self, category_slug: str ):
         try:
             url = f'{self.base_url}/product-category/{category_slug}/'
-            context = ssl._create_unverified_context()
-            response = urllib.request.urlopen(url, context=context)
+            response = urllib.request.urlopen(url, context=self.context)
             return json.loads(response.read().decode('utf-8'))
 
-        except requests.RequestException as e:
+        except Exception as e:
             print(f'Error al obtener productos por categoría: {str(e)}')
             return []  # Devolver lista vacía en caso de error
